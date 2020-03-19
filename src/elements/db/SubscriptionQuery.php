d94053dccc237f4f9922c3ebf4b62242c06f3e1b
Jan Henckens <jan@studioespresso.co>
2020-03-19 15:22:06 +0100

    Subscription element

---
+++ src/elements/db/SubscriptionQuery.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace studioespresso\molliesubscriptions\elements\db;
+
+use craft\elements\db\ElementQuery;
+use craft\helpers\Db;
+
+class SubscriptionQuery extends ElementQuery
+{
+
+    public $plan;
+
+    public $subscriptionStatus;
+
+    public $email;
+
+    public function email($value)
+    {
+        $this->email = $value;
+        return $this;
+    }
+
+    public function status($value)
+    {
+        $this->subscriptiontStatus = $value;
+        return $this;
+    }
+
+
+    public function subscriptiontStatus($value)
+    {
+        $this->subscriptiontStatus = $value;
+        return $this;
+    }
+
+    public function plan($value)
+    {
+        $this->plan = $value;
+        return $this;
+    }
+
+    protected function statusCondition(string $status)
+    {
+        switch ($status) {
+            case 'pending':
+                return ['subscriptiontStatus' => 'pending'];
+            case 'paid':
+                return ['subscriptiontStatus' => 'paid'];
+            case 'expired':
+                return ['subscriptiontStatus' => 'expired'];
+            default:
+                return parent::statusCondition($status);
+        }
+    }
+
+
+    protected function beforePrepare(): bool
+    {
+        $this->joinElementTable('{{%mollie_subscriptions}}');
+        // select the columns
+        $this->query->select([
+            'mollie_subscriptions.email',
+            'mollie_subscriptions.amount',
+            'mollie_subscriptions.plan',
+            'mollie_subscriptions.paymentStatus',
+        ]);
+
+        if ($this->email) {
+            $this->subQuery->andWhere(Db::parseParam('mollie_subscriptions.email', $this->email));
+        }
+
+        if ($this->plan) {
+            $this->subQuery->andWhere(Db::parseParam('mollie_subscriptions.plan', $this->formId));
+        }
+
+        if ($this->subscriptionStatus) {
+            $this->subQuery->andWhere(Db::parseParam('mollie_subscriptions.subscriptionStatus', $this->status));
+        }
+
+        return parent::beforePrepare();
+    }
+}
